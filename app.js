70556cb17237814e480123535666c3cb711278c0
savva <savva.lambin@gmail.com>
2023-07-11 08:20:18 +0100

    Add TypeScript

---
+++ app.js
@@ -0,0 +1,7 @@
+// Define a function that takes two numbers and returns their sum
+function addNumbers(a, b) {
+    return a + b;
+}
+// Call the function
+var result = addNumbers(6, 3);
+console.log(result); // Output: 8
---
+++ app.ts
@@ -0,0 +1,8 @@
+// Define a function that takes two numbers and returns their sum
+function addNumbers(a: number, b: number): number {
+    return a + b;
+  }
+  
+  // Call the function
+  const result = addNumbers(6, 3);
+  console.log(result); // Output: 8
